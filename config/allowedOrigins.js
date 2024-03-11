7ac2585cd52badb93dbd07cedd950480be370a0a
dskkoji <50835363+dskkoji@users.noreply.github.com>
2024-03-11 14:34:34 +0900

    Ready to Deploy backend

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+  'https://technotes.onrender.com',
+  // 'http://localhost:3000',
+  // 'https://www.google.com',
+  // 'https://www.dandrepairshop.com',
+  // 'https://dandrepairshop.com'
+]
+module.exports = allowedOrigins
